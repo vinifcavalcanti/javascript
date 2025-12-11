6146e667e153c0ed7e99ff228bcfe31e4c4170ea
Vinícius  Fuentes Cavalcanti <142112837+vinifcavalcanti@users.noreply.github.com>
2025-12-11 13:21:02 -0300

    ex

---
+++ exercicios/aula12/ex012.js
@@ -0,0 +1,12 @@
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora < 12 && hora >= 6) {
+    console.log('Bom dia!')
+} else if(hora <= 18 && hora >= 13){
+    console.log('Boa tarde!')
+}else if(hora <= 23 && hora >= 19){
+    console.log('Boa noite!')
+}else{
+    console.log('Boa madrugada!')
+}
