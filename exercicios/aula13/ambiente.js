bcab118811aae683063f284bcb1beb861edae9dc
Vinícius  Fuentes Cavalcanti <142112837+vinifcavalcanti@users.noreply.github.com>
2025-12-15 12:25:49 -0300

    Create ambiente.js

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,25 @@
+var c = 1
+while(c <= 6){
+    console.log(`Passo ${c}`)
+    c++ 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
