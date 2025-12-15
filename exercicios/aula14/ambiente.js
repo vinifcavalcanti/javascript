4e1ec5263b4bb6b868e791dae10a1bec3ee0d6f7
Vinícius  Fuentes Cavalcanti <142112837+vinifcavalcanti@users.noreply.github.com>
2025-12-15 12:46:12 -0300

    Update ambiente.js

--- exercicios/aula14/ambiente.js
+++ exercicios/aula14/ambiente.js
@@ -1,10 +1,5 @@
-/* var c = 1
-while(c <= 5){
-    console.log(c)
-    c++
-}
-*/
-
-for(var c = 1; c <=5; c++){
+console.log(`vai começar...`)
+for(var c = 1; c <=4; c++){
     console.log(c)
 }
+console.log('Fim')
