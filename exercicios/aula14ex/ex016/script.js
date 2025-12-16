2ea3f2bc3b924c84048ea6e20180ae533302bed5
Vinícius  Fuentes Cavalcanti <142112837+vinifcavalcanti@users.noreply.github.com>
2025-12-16 11:33:47 -0300

    Update script.js

--- exercicios/aula14ex/ex016/script.js
+++ exercicios/aula14ex/ex016/script.js
@@ -2,10 +2,29 @@ function contar(){
     let ini = document.getElementById('txti')
     let fim = document.getElementById('txtf')
     let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
-    if(ini.value.lenght == 0 || fim.value.lenght == 0 || passo.value.lenght == 0){
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar!'
         window.alert('[ERRO] Faltam dados!')
     }else{
-        alert('Tudo OK!')
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(p <= 0){
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1 
+        }
+        if(i < f) {
+            for(let c = i; c <= f; c += p){
+            res.innerHTML += `${c} \u{1F449}`
+        }
+        }else{
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3c1}`
     }
 }
