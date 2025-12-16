d11e68322694112878b6d3595defd0d79f5f69b1
Vinícius  Fuentes Cavalcanti <142112837+vinifcavalcanti@users.noreply.github.com>
2025-12-16 11:20:25 -0300

    ex16

--- exercicios/aula14ex/ex016/script.js
+++ exercicios/aula14ex/ex016/script.js
@@ -1,1 +1,11 @@
-alert('Óla')
+function contar(){
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+
+    if(ini.value.lenght == 0 || fim.value.lenght == 0 || passo.value.lenght == 0){
+        window.alert('[ERRO] Faltam dados!')
+    }else{
+        alert('Tudo OK!')
+    }
+}
