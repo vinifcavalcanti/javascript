b7dbb63656de0d7b569279903fcdfa11150b94f3
Vinícius  Fuentes Cavalcanti <142112837+vinifcavalcanti@users.noreply.github.com>
2025-12-17 12:22:14 -0300

    funcao

---
+++ exercicios/aula15/ambiente.js
@@ -0,0 +1,14 @@
+let num = [5, 8, 2, 9 ,3]
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`Nosso vetor é o ${num.length}`)
+console.log(`O nosso primeiro vetor é ${num[0]}`)
+let pos = num.indexOf(2)
+if(pos == -1){
+    console.log('O valor não foi encontrado')
+}else{
+    console.log(`O valor está na posição ${pos}`)
+}
+
+
